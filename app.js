de3943df753c43b07e8fdcfe04f0481a954ae708
JisooArchive <jisoo.cheh@gmail.com>
2025-02-25 21:39:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ class App {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d')
         document.body.appendChild(this.canvas);
-
         this.waveGroup = new WaveGroup()
-
         window.addEventListener('resize', this.resize.bind(this, false))
         this.resize();
         
