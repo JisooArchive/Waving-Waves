8a93f56a697a6789131a4263024e5448190acfa0
JisooArchive <jisoo.cheh@gmail.com>
2025-03-01 22:12:18 +0900

    Update point.js

--- point.js
+++ point.js
@@ -3,6 +3,7 @@ export class Point {
         this.x = x;
         this.y = y; 
         this.fixedY = y;
+        
         this.speed = 0.07;
         this.cur = index
         this.max = Math.random() * 100 + 150
